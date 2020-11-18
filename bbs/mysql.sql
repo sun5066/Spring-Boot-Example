abe1a2cb2e40e378eda1c6614ea1729482f93d74
sun5066 <alstjr4434@naver.com>
2020-11-18 16:42:32 +0900

    11-18

---
+++ bbs/mysql.sql
@@ -0,0 +1,15 @@
+use mysql;
+show tables ;
+
+SHOW databases;
+
+CREATE DATABASE mybbs;
+SHOW databases;
+use mybbs;
+SHOW TABLES;
+DESC tbl_bbs;
+
+INSERT INTO tbl_bbs(b_writer,b_date_time,b_content,b_count)
+values ('으용이가','2020-11-18','게시판만들깅',0);
+
+SELECT * FROM tbl_bbs;
